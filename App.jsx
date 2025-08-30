c4d6de686ec6226f086738887d320feae30d3307
vickypirdhan <vickypirdhan40@gmail.com>
2025-08-30 12:12:49 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,1 @@
 
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-primary">Dheeraj Premium Portfolio</h1>
-    </div>
-  )
-}
