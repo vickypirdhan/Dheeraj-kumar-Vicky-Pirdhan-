9094e3b34f270daaa66ae5b985f2ded8ce170ff4
vickypirdhan <vickypirdhan40@gmail.com>
2025-08-30 12:13:29 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,1 @@
 
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: ['class'],
-  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        background: '#0b0b12',
-        foreground: '#f5f7ff',
-        primary: {
-          DEFAULT: '#7c3aed',
-        },
-      }
-    }
-  },
-  plugins: []
-}
