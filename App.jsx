80180b7844f3e76c6d39c86271597bfed63997f9
vickypirdhan <vickypirdhan40@gmail.com>
2025-08-30 12:10:05 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-primary">Dheeraj Premium Portfolio</h1>
+    </div>
+  )
+}
